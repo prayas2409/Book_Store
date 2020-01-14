b9a2e41095123bfa846db069ef273f25019c719a
jitendraB <jitupatil937@gmail.com>
2020-01-14 15:44:57 +0530

    Added database configuration.

---
+++ config/db.connection.js
@@ -0,0 +1,13 @@
+const dbConfig = require('./db.config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
