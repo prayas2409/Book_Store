73860ea7855fec099a9b4b5574a654dcf2a2e015
GaikwadRavi03 <gaikwadr576@gmail.com>
2020-01-15 19:04:13 +0530

    Refactored test cases for sorting.

--- test/controller/controllerTest.js
+++ test/controller/controllerTest.js
@@ -7,18 +7,18 @@ chai.use(chaiHttp);
 
 describe(`book list`, () => {
 
-    it(`given a books  When  all books details are proper then return 200 status code`, (done) => {
-        chai.request(app)
-            .post('/addBook')
-            .send(sampleJSON.addBook200)
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                }
-                res.should.have.status(200);
-                done();
-            });
-    });
+    // it(`given a books  When  all books details are proper then return 200 status code`, (done) => {
+    //     chai.request(app)
+    //         .post('/addBook')
+    //         .send(sampleJSON.addBook200)
+    //         .end((err, res) => {
+    //             if (err) {
+    //                 return done(err);
+    //             }
+    //             res.should.have.status(200);
+    //             done();
+    //         });
+    // });
 
     it(`given a books  When  all books details are not proper then return 422 status code`, (done) => {
         chai.request(app)
@@ -72,6 +72,8 @@ describe(`describe Mocha Test for Book store`, () => {
                     res.body.data[0].id.should.be.eql("1");
                     res.body.data[0].author.should.be.eql("Chetan Bhagat'");
                     res.body.data[0].title.should.be.eql("The Girl in Room 105'");
+                    res.body.data[0].quantity.should.be.eql(12);
+                    res.body.data[0].price.should.be.eql(193);
                     res.should.have.status(200);
                     done()
                 }
@@ -81,13 +83,14 @@ describe(`describe Mocha Test for Book store`, () => {
     it(`should return last book data when book store database found this book.`, (done) => {
         chai.request(app).get('/books')
             .end((err, res) => {
-                // console.log('=====>',res.body)
                 if (err) {
-                    err.should.have.status(404)
+                    err.should.have.status(400)
                 } else {
                     res.body.data[51].id.should.be.eql("52");
                     res.body.data[51].author.should.be.eql("Stephen King'");
                     res.body.data[51].title.should.be.eql("Doctor Sleep'");
+                    res.body.data[51].quantity.should.be.eql(14);
+                    res.body.data[51].price.should.be.eql(245);
                     res.should.have.status(200);
                     done()
                 }
@@ -96,7 +99,7 @@ describe(`describe Mocha Test for Book store`, () => {
 
     it(`given a list of books When wrong url then return 404 status code`, (done) => {
         chai.request(app)
-            .post('/books')
+            .post('/bookz')
             .end((err, res) => {
                 if (err) {
                     return done(err);
@@ -149,6 +152,14 @@ describe(`describe Mocha Test for sorting books`, () => {
         chai.request(app).get('/sortBooks').send(sampleJSON.sortBook400)
             .end((err, res) => {
                 res.should.have.status(400);
+                done()
+            })
+    });
+
+    it(`should return status code 404 when url is wrong.`, (done) => {
+        chai.request(app).get('/sortBookz').send(sampleJSON.sortBook400)
+            .end((err, res) => {
+                res.should.have.status(404);
                 done()
             })
     });
